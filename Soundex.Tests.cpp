84b5774318891af70943528e86c61e45a0278fef
RakshithR98 <rakshithraghu2017@gmail.com>
2024-09-05 19:29:45 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -10,3 +10,14 @@ TEST(SoundexTest, HandlesSingleCharacter) {
     EXPECT_EQ(generateSoundex("A"), "A000");
 }
 
+TEST(SoundexTest, HandlesMixedCase) {
+    EXPECT_EQ(generateSoundex("RaJesh"), "R020");
+    EXPECT_EQ(generateSoundex("MAHESH"), "M020");
+}
+
+TEST(SoundexTest, HandlesSpecialCharacters) {
+    EXPECT_EQ(generateSoundex("O'Reilly"), "O640");
+    EXPECT_EQ(generateSoundex("Ravi-Kumar"), "R251");
+}
+
+
